30f7b47c14640e01a99b138775933d3892b829a9
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2018-01-24 19:12:53 +0100

    Remove cyclic dependencies

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from active_learning.active_learning import ActiveLearner
-from domain_scoring.domain_scoring import DomainScoring
-from explanation.explanation import Explanation
+import active_learning
+import domain_scoring
+import explanation
 import argparse
 
 RESEARCH_MODE = "research"
@@ -21,13 +21,13 @@ if __name__ == '__main__':
     args = parse_arguments()
 
     # beta: active learning of relevant meta-paths
-    active_learner = ActiveLearner()
+    active_learner = active_learning.ActiveLearner()
     rated_paths = active_learner.retrieve_user_rating()
 
     # gamma: score the learned paths
-    domain_score = DomainScoring(rated_paths, mode=args.mode)
+    domain_score = domain_scoring.DomainScoring(rated_paths, mode=args.mode)
 
     # delta
-    explanation = Explanation()
+    explanation = explanation.Explanation()
 
     print("...did everything.")
