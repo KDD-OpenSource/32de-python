eb86c3ce5d0ae5d3011fc0a3df88b1f244bf64b8
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2018-01-24 21:17:52 +0100

    Add routes for API

--- api/server.py
+++ api/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
 
 app = Flask(__name__)
 
@@ -7,6 +7,34 @@ def run(port, hostname, debug_mode):
     app.run(host=hostname, port=port, debug=debug_mode)
 
 
-@app.route("/")
-def hello_world():
+@app.route("/node-sets", options="POST")
+def receive_node_sets():
+    # TODO: Check if necessary information is in request object
+    if not request.json:
+        abort(400)
+
+
+@app.route("/types", options="POST")
+def receive_edge_node_types():
+    # TODO: Check if necessary information is in request object
+    if not request.json:
+        abort(400)
+
+
+@app.route("/next-meta-paths", options="GET")
+def send_next_metapaths_to_rate():
+    # TODO: Call fitting method in active_learning
+    return jsonify("Hello world")
+
+
+@app.route("/next-meta-paths", options="POST")
+def receive_rated_metapaths():
+    # TODO: Check if necessary information is in request object
+    if not request.json:
+        abort(400)
+
+
+@app.route("/results", options="GET")
+def send_results():
+    # TODO: Call fitting method in explanation
     return jsonify("Hello world")
