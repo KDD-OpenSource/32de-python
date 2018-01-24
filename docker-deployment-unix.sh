933844358da7e383c132ba7953016cfa49cb9b5d
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2018-01-24 23:57:41 +0100

    Correct bash script

--- docker-deployment-unix.sh
+++ docker-deployment-unix.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
+mkdir tmp/
+cd tmp/
 docker run \
     --publish=7474:7474 --publish=7687:7687 \
     --volume=$HOME/neo4j/data:/data \
     --volume=$HOME/neo4j/logs:/logs \
     --env=NEO4J_ACCEPT_LICENSE_AGREEMENT=yes \
-    --rm \
     -d \
+    --name neo4j-container \
     neo4j:3.3-enterprise
 
-# How to integrate our neo4j code changes (meta-path computation)?
+# TODO: How to integrate our neo4j code changes (meta-path computation)?
 # TODO: Remove -b when on master
 git clone -b configure-docker https://github.com/KDD-OpenSource/32de-python.git
 cd 32de-python/
 docker build -t server -f Dockerfile .
-docker run server --name server-container --publish=8000:8000 --rm -d
+docker run --name server-container \
+           --publish=8000:8000 \
+           -d \
+           server
 cd ..
 
 git clone https://github.com/KDD-OpenSource/32de-UI.git
 cd 32de-UI/
 docker build -t ui -f Dockerfile .
-docker run ui --name ui-container --publish=80:80 --rm -d
+docker run --name ui-container \
+           --publish=80:80 \
+           -d \
+           ui
 cd ..
+read -p "Press enter to terminate the containers"
+cd ..
+rm -rf tmp/
