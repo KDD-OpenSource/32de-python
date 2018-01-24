418a0507ba8b74031f9049323693f29c60a91a74
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2018-01-24 10:42:12 +0100

    Add documentation of argument to DomainScoring

--- domain_scoring/domain_scoring.py
+++ domain_scoring/domain_scoring.py
@@ -1,7 +1,15 @@
 from explanation.explanation import Explanation
+from typing import List
+
 
 class DomainScoring():
-    def __init__(self):
+    def __init__(self, weighted_metapaths: List):
+        """
+
+        :param weighted_metapaths: A list of lists, each inner list element represents one user weighting round.
+        Each user weighting is itself a list where we care about the ordering (represents ordering on UI) and
+        it contains the distance to the next element.
+        """
         raise NotImplementedError()
 
     def score(self):
