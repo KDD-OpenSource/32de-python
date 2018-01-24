ce01aa25cd6a18d44ff65bce4b00fd3aee4c542c
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2018-01-24 20:37:51 +0100

    Add flask server

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import active_learning
 import domain_scoring
 import explanation
+from api import server
 import argparse
 
 RESEARCH_MODE = "research"
@@ -11,6 +12,17 @@ def parse_arguments():
     parser.add_argument("--mode",
                         choices=[BASELINE_MODE, RESEARCH_MODE],
                         help="Run the baseline or our research prototype")
+    parser.add_argument("--port",
+                        type=int,
+                        default=5000,
+                        help="Port the flask server should listen on")
+    parser.add_argument("--host",
+                        type=str,
+                        default='127.0.0.1',
+                        help="Hostname the flask server should listen on")
+    parser.add_argument("--debug",
+                        action="store_true",
+                        help="Debug mode of flask server")
 
     return parser.parse_args()
 
@@ -19,6 +31,8 @@ if __name__ == '__main__':
     print("Starting system...")
 
     args = parse_arguments()
+
+    server.run(port=args.port, hostname=args.host, debug_mode=args.debug)
 
     # beta: active learning of relevant meta-paths
     active_learner = active_learning.ActiveLearner()
