7d97d64cf2ad7c8b85c5344b776398eff76ebe09
juliusrueckin <julius.rueckin@rats-os.de>
2018-01-25 14:38:04 +0100

    extend meta path datastructure for structural and domain score

--- util/datastructures.py
+++ util/datastructures.py
@@ -1,6 +1,9 @@
 class MetaPath:
+
     edges = None
     nodes = None
+    structural_score = None
+    domain_score = None
 
     def __init__(self):
         self.edges = []
@@ -10,6 +13,14 @@ class MetaPath:
         assert (len(nodes) - 1 == len(edges)) or (len(nodes) == 0 and len(edges) == 0), "Path is not valid, number of edges and nodes does not compse a path"
         self.edges = edges
         self.nodes = nodes
+
+    def __init__(self, nodes, edges, structural_score, domain_score):
+        assert (len(nodes) - 1 == len(edges)) or (len(nodes) == 0 and len(edges) == 0), "Path is not valid, number of edges and nodes does not compse a path"
+        assert (structural_score is not None and domain_score is not None), "Structural score and domain score have to be set"
+        self.edges = edges
+        self.nodes = nodes
+        self.structural_score = structural_score
+        self.domain_score = domain_score
 
     def is_empty(self):
         return len(self) == 0
