994ab50131812781651b65c9ca5c50a686a0affd
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2018-01-25 10:45:56 +0100

    Add building of our changes in neo4j-graph-algorithms

--- docker-deployment-unix.sh
+++ docker-deployment-unix.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 mkdir tmp/
 cd tmp/
+
+# TODO: Remove -b when on master
+git clone -b configure-docker https://github.com/KDD-OpenSource/32de-python.git
+cd 32de-python/
+
+docker build -t neo4j -f Dockerfile-neo4j-graph-algorithms .
 docker run \
     --publish=7474:7474 --publish=7687:7687 \
     --volume=$HOME/neo4j/data:/data \
     --volume=$HOME/neo4j/logs:/logs \
+    --volume=$HOME/neo4j/plugins:/plugins \
     --env=NEO4J_ACCEPT_LICENSE_AGREEMENT=yes \
     -d \
     --name neo4j-container \
-    neo4j:3.3-enterprise
+    neo4j
 
-# TODO: How to integrate our neo4j code changes (meta-path computation)?
-# TODO: Remove -b when on master
-git clone -b configure-docker https://github.com/KDD-OpenSource/32de-python.git
-cd 32de-python/
 docker build -t server -f Dockerfile .
 docker run --name server-container \
            --publish=8000:8000 \
