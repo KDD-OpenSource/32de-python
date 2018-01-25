5b5998e711c0103e0ec99a8fbcac032a2290b0ec
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2018-01-25 09:41:21 +0100

    Add extra route and TODOs

--- api/server.py
+++ api/server.py
@@ -7,6 +7,7 @@ def run(port, hostname, debug_mode):
     app.run(host=hostname, port=port, debug=debug_mode)
 
 
+# TODO: If meta-paths for A and B will be written in Java, they will need this information in Java
 @app.route("/node-sets", options="POST")
 def receive_node_sets():
     # TODO: Check if necessary information is in request object
@@ -14,6 +15,13 @@ def receive_node_sets():
         abort(400)
 
 
+@app.route("/node-sets", options="GET")
+def send_node_sets():
+    # TODO: Call fitting method in active_learning
+    return jsonify("Hello world")
+
+
+# TODO: If meta-paths for A and B will be written in Java, they will need this information in Java
 @app.route("/types", options="POST")
 def receive_edge_node_types():
     # TODO: Check if necessary information is in request object
@@ -27,6 +35,7 @@ def send_next_metapaths_to_rate():
     return jsonify("Hello world")
 
 
+# TODO: Maybe post each rated meta-path
 @app.route("/next-meta-paths", options="POST")
 def receive_rated_metapaths():
     # TODO: Check if necessary information is in request object
