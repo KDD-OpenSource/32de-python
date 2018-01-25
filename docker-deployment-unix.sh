fcde13e686a9452a2068ff282c8d02fb5aa939ff
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2018-01-25 11:11:33 +0100

    Set --no-cache option, otherwise our new code isn't deployed

--- docker-deployment-unix.sh
+++ docker-deployment-unix.sh
@@ -6,7 +6,8 @@ cd tmp/
 git clone -b configure-docker https://github.com/KDD-OpenSource/32de-python.git
 cd 32de-python/
 
-docker build -t neo4j-graph-algo -f Dockerfile-neo4j-graph-algorithms .
+# TODO: Is there a nicer way than --no-cache? Maybe clone outside
+docker build --no-cache -t neo4j-graph-algo -f Dockerfile-neo4j-graph-algorithms .
 docker run \
     --publish=7474:7474 --publish=7687:7687 \
     --volume=$HOME/neo4j/data:/data \
@@ -17,7 +18,7 @@ docker run \
     --name neo4j-graph-algo-container \
     neo4j-graph-algo
 
-docker build -t server -f Dockerfile .
+docker build --no-cache -t server -f Dockerfile .
 docker run --name server-container \
            --publish=8000:8000 \
            -d \
@@ -26,7 +27,7 @@ cd ..
 
 git clone https://github.com/KDD-OpenSource/32de-UI.git
 cd 32de-UI/
-docker build -t ui -f Dockerfile .
+docker build --no-cache -t ui -f Dockerfile .
 docker run --name ui-container \
            --publish=80:80 \
            -d \
