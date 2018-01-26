b1b0cced1fa58b996d6e1bff076430595a787494
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2018-01-26 16:00:31 +0100

    Correct arguments

--- deployment/build-neo4j.sh
+++ deployment/build-neo4j.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t neo4j-graph-algorithms -f Dockerfile ..
+docker build -t neo4j-graph-algorithms -f Dockerfile .
--- deployment/build-ui.sh
+++ deployment/build-ui.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t ui -f Dockerfile ..
+docker build -t ui -f Dockerfile .
