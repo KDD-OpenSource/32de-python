121f1af0858230578b1bbfd7482a4be0d8991076
Freya Behrens <freya@fitt-behrens.de>
2018-02-19 23:00:27 +0100

    Assume server start from root directory

--- util/config.py
+++ util/config.py
@@ -7,13 +7,10 @@ BASELINE_MODE = "baseline"
 REACT_PORT = 3000
 API_PORT = 8000
 # Data sets
-# TODO review this
-# all these paths are relative to the server.py script
-# your os.getcwd() should be "api/"
-ROTTEN_TOMATO_PATH = os.path.join('..', 'tests', 'data', 'rotten_tomatoes')
-RATED_DATASETS_PATH = os.path.join('..', 'rated_datasets')
-MOCK_DATASETS_DIR = os.path.join('..', 'tests', 'data')
+ROTTEN_TOMATO_PATH = os.path.join('tests', 'data', 'rotten_tomatoes')
+RATED_DATASETS_PATH = os.path.join('rated_datasets')
+MOCK_DATASETS_DIR = os.path.join('tests', 'data')
 # Configuration for sessions saved on the file system
-SESSION_CACHE_DIR = os.path.join('..', 'tmp', 'sessions')
+SESSION_CACHE_DIR = os.path.join('tmp', 'sessions')
 SESSION_THRESHOLD = 500
 SESSION_MODE = '0700'
