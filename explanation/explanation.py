cbb41573a70aeb22f7ce3cf92bb84a2b69975fe2
Julius Rückin <julius.rueckin@student.hpi.uni-potsdam.de>
2018-02-19 11:19:49 +0100

    Refactor code with regard to reviews

--- explanation/explanation.py
+++ explanation/explanation.py
@@ -1,14 +1,8 @@
-from util.datastructures import MetaPath, MetaPathRating
+from typing import List
+from util.datastructures import MetaPathRating
+from util.config import BASELINE_MODE
 
 import numpy as np
-
-# mock data
-mock_paths = [MetaPathRating(MetaPath([1, 2], [3]), 3, 1), MetaPathRating(MetaPath([1, 2, 1], [3, 4]), 1, 3),
-              MetaPathRating(MetaPath([1, 3, 2], [1, 1]), 5, 2), MetaPathRating(MetaPath([1, 3, 2], [1, 1]), 4, 7),
-              MetaPathRating(MetaPath([1, 3, 2], [1, 1]), 6, 3)]
-
-# algorithm types
-BASELINE = 'baseline'
 
 
 class Explanation:
@@ -19,16 +13,27 @@ class Explanation:
 class SimilarityScore:
     meta_paths = None
     similarity_score = None
+    algorithm_type = None
 
-    def __init__(self, algorithm_type=BASELINE):
+    def __init__(self, algorithm_type=BASELINE_MODE):
         self.algorithm_type = algorithm_type
 
+    '''
+    Later, we will fetch meta-paths dynamically. At the moment, we pass meta paths directly
+    from the tests.
+    '''
     @staticmethod
-    def fetch_meta_paths(self):
-        return mock_paths
+    def fetch_meta_paths(self) -> List[MetaPathRating]:
+        return self.meta_paths
 
+
+    '''
+    Computes a sum of a linear combination of structural and domain value
+    over all meta-paths, normalized by each meta-path length. First simplified,
+    not experimentally tested baseline.
+    '''
     @staticmethod
-    def calculate_similarity(meta_path_ratings):
+    def calculate_similarity(meta_path_ratings: List[MetaPathRating]) -> float:
         structural_values = np.array([])
         domain_values = np.array([])
 
@@ -37,5 +42,3 @@ class SimilarityScore:
             domain_values = np.append(domain_values, [meta_path_rating.domain_value])
 
         return np.sum(structural_values * domain_values) / len(meta_path_ratings)
-
-
