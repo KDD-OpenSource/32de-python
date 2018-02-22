2d245a11e7ae8fce74d4f9bae73b9eebb50948fd
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2018-02-23 00:40:50 +0100

    Add skeleton for intents from Flask Ask

--- api/server.py
+++ api/server.py
@@ -9,8 +9,10 @@ import json
 import os
 import time
 import datetime
+from flask_ask import Ask
 
 app = Flask(__name__)
+ask = Ask(app, '/alexa')
 
 # TODO: Change if we have a database in the background
 SESSION_TYPE = 'filesystem'
@@ -228,6 +230,107 @@ def send_results():
     """
     # TODO: Call fitting method in explanation
     raise NotImplementedError("This API endpoint isn't implemented in the moment")
+
+
+# Self defined intents
+@ask.intent('ChooseDataset')
+def choose_dataset(dataset):
+    raise NotImplementedError()
+
+
+@ask.intent('RateMetapath')
+def rate_metapath():
+    raise NotImplementedError()
+
+
+@ask.intent('ExcludeEdgeType')
+def exclude_edge_type():
+    raise NotImplementedError()
+
+
+@ask.intent('ExcludeNodeType')
+def exclude_node_type():
+    raise NotImplementedError()
+
+
+@ask.intent('ShowMoreMetapaths')
+def show_more_metapaths():
+    raise NotImplementedError()
+
+
+@ask.intent('ShowResults')
+def show_results():
+    raise NotImplementedError()
+
+# Built-in intents
+@ask.intent('AMAZON.CancelIntent')
+def cancel():
+    raise NotImplementedError()
+
+
+@ask.intent('AMAZON.HelpIntent')
+def help():
+    raise NotImplementedError()
+
+
+@ask.intent('AMAZON.StopIntent')
+def stop():
+    raise NotImplementedError()
+
+
+@ask.intent('AMAZON.MoreIntent')
+def more():
+    raise NotImplementedError()
+
+
+@ask.intent('AMAZON.NavigateHomeIntent')
+def navigate_home():
+    raise NotImplementedError()
+
+
+@ask.intent('AMAZON.NavigateSettingsIntent')
+def navigate_settings():
+    raise NotImplementedError()
+
+
+@ask.intent('AMAZON.NextIntent')
+def next():
+    raise NotImplementedError()
+
+
+@ask.intent('AMAZON.PageUpIntent')
+def page_up():
+    raise NotImplementedError()
+
+
+@ask.intent('AMAZON.PageDownIntent')
+def page_down():
+    raise NotImplementedError()
+
+
+@ask.intent('AMAZON.PreviousIntent')
+def previous():
+    raise NotImplementedError()
+
+
+@ask.intent('AMAZON.ScrollRighIntent')
+def scroll_right():
+    raise NotImplementedError()
+
+
+@ask.intent('AMAZON.ScrollDownIntent')
+def scroll_down():
+    raise NotImplementedError()
+
+
+@ask.intent('AMAZON.ScrollLeftIntent')
+def scroll_left():
+    raise NotImplementedError()
+
+
+@ask.intent('AMAZON.ScrollUpIntent')
+def scroll_up():
+    raise NotImplementedError()
 
 
 if __name__ == '__main__':
