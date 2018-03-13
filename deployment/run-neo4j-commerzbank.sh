4e73b67d0110608fca5445128c3fd3d21f1825be
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2018-03-13 10:56:06 +0100

    Clean up normal run scripts and create visualvm scripts

--- deployment/run-neo4j-commerzbank.sh
+++ deployment/run-neo4j-commerzbank.sh
@@ -2,21 +2,11 @@
 docker stop neo4j-graph-algo-container-commerzbank
 docker rm neo4j-graph-algo-container-commerzbank
 docker run \
-    --publish=7494:7474 --publish=7707:7687 --publish=3636:3636\
+    --publish=7494:7474 --publish=7707:7687\
     --volume=$HOME/commerzbank/data:/data \
     --volume=$HOME/commerzbank/logs:/logs \
     --volume=$HOME/commerzbank/conf:/conf \
     --env=NEO4J_ACCEPT_LICENSE_AGREEMENT=yes \
-    --env=NEO4J_dbms_memory_pagecache_size=40G \
-    --env=NEO4J_dbms_memory_heap_initial__size=50G \
-    --env=NEO4J_dbms_memory_heap_max__size=200G \
-    --env=-Djava.rmi.server.hostname=172.20.14.22 \
-    --env=-Dcom.sun.management.jmxremote.rmi.port=3636 \
-    --env=-Dcom.sun.management.jmxremote \
-    --env=-Dcom.sun.management.jmxremote.port=3636 \
-    --env=-Dcom.sun.management.jmxremote.local.only=false \
-    --env=-Dcom.sun.management.jmxremote.authenticate=false \
-    --env=-Dcom.sun.management.jmxremote.ssl=false \
     --env=NEO4J_AUTH=neo4j/${COMMERZBANK_PWD} \
     -d \
     --name neo4j-graph-algo-container-commerzbank \
--- deployment/run-neo4j-helmholtz.sh
+++ deployment/run-neo4j-helmholtz.sh
@@ -2,18 +2,11 @@
 docker stop neo4j-graph-algo-container-helmholtz
 docker rm neo4j-graph-algo-container-helmholtz
 docker run \
-    --publish=7484:7474 --publish=7697:7687 --publish=3637:3637 --publish=1099:1099 \
+    --publish=7484:7474 --publish=7697:7687 \
     --volume=$HOME/bioDB/data:/data \
     --volume=$HOME/bioDB/logs:/logs \
     --volume=$HOME/bioDB/conf:/conf \
     --env=NEO4J_ACCEPT_LICENSE_AGREEMENT=yes \
-    --env=-Djava.rmi.server.hostname=172.20.14.22 \
-    --env=-Dcom.sun.management.jmxremote.rmi.port=3637 \
-    --env=-Dcom.sun.management.jmxremote \
-    --env=-Dcom.sun.management.jmxremote.port=3637 \
-    --env=-Dcom.sun.management.jmxremote.local.only=false \
-    --env=-Dcom.sun.management.jmxremote.authenticate=false \
-    --env=-Dcom.sun.management.jmxremote.ssl=false \
     --env=NEO4J_AUTH=neo4j/${HELMHOLTZ_PWD} \
     -d \
     --name neo4j-graph-algo-container-helmholtz \
