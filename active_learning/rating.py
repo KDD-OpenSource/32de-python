5665d2cf98978efefb1f87bdd1004b3b11785e88
Freya Behrens <freya@fitt-behrens.de>
2018-03-15 12:53:48 +0100

    Add random Oracle

--- active_learning/rating.py
+++ active_learning/rating.py
@@ -1,4 +1,5 @@
 from util.datastructures import MetaPath
+import numpy as np
 
 """
 A collection of functions for rating a domain-based score of a meta-path.
@@ -10,3 +11,9 @@ def length_based(mp: MetaPath) -> float:
     Set the rating as the length of the metapath.
     """
     return float(len(mp))
+
+def randomly(mp: MetaPath) -> float:
+    """
+    Rate the metapath at a random valuein [0,1].
+    """
+    return np.random.rand()
