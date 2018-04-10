506d59f9156941c4ee6e0670f5690a33df3bdc1c
Julius Rückin <julius.rueckin@student.hpi.uni-potsdam.de>
2018-04-10 12:07:57 +0200

    fix meta-path ids

--- api/server.py
+++ api/server.py
@@ -246,43 +246,43 @@ def send_similarity_score():
 def send_contributing_meta_paths():
     contributing_meta_paths = [
       {
-        "id": "make",
+        "id": 1,
         "label": "make",
         "value": 551,
         "color": "hsl(131, 70%, 50%)"
       },
       {
-        "id": "erlang",
+        "id": 2,
         "label": "erlang",
         "value": 226,
         "color": "hsl(358, 70%, 50%)"
       },
       {
-        "id": "c",
+        "id": 3,
         "label": "c",
         "value": 129,
         "color": "hsl(151, 70%, 50%)"
       },
       {
-        "id": "php",
+        "id": 4,
         "label": "php",
         "value": 67,
         "color": "hsl(52, 70%, 50%)"
       },
       {
-        "id": "java",
+        "id": 5,
         "label": "java",
         "value": 452,
         "color": "hsl(221, 70%, 50%)"
       },
       {
-        "id": "stylus",
+        "id": 6,
         "label": "stylus",
         "value": 406,
         "color": "hsl(102, 70%, 50%)"
       },
       {
-        "id": "ruby",
+        "id": 7,
         "label": "ruby",
         "value": 433,
         "color": "hsl(341, 70%, 50%)"
