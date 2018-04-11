25f3257d10773ab02def1d7760dd0cd29d746279
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2018-04-11 21:03:29 +0200

    Add word2vec cbow

--- embeddings/meta2vec.py
+++ embeddings/meta2vec.py
@@ -49,8 +49,39 @@ def model_word2vec_skipgram():
     pass
 
 
-def model_word2vec_cbow():
-    pass
+def model_word2vec_cbow(mode, size_of_vocabulary: int, loss: str):
+    labels = None
+    embeddings = None
+    ids = None
+
+    # Look up embedding for all words
+    embedded_words = tf.nn.embedding_lookup(embeddings, ids)
+    # Concatenate vectors
+    concatenated_embeddings = embedded_words
+    # Transform embeddings linearly
+    hidden_layer = tf.layers.dense(inputs=concatenated_embeddings, units=size_of_vocabulary, activation=None,
+                                   use_bias=True,
+                                   name="linear_transformation",
+                                   kernel_initializer=tf.random_uniform_initializer(minval=-1, maxval=1))
+
+    # Apply softmax and calulate loss
+    if loss == 'cross_entropy':
+        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=hidden_layer, labels=labels),
+                              name="cross_entropy_loss")
+    else:
+        raise NotImplementedError("This loss isn't implemented yet. Implement it!")
+
+    optimizer = tf.train.AdamOptimizer()
+    total_train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
+
+    metrics = {'loss': loss}
+    # Loss for tensorboard
+    tf.summary.scalar('Loss', loss)
+
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=total_train_op)
+    else:
+        raise NotImplementedError("This mode isn't implemented yet")
 
 
 def model_paragraph_vectors_skipgram():
