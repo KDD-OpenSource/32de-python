8347a8b763fcab50b3f3c7e11cc61fa2b88671db
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2018-04-11 13:43:59 +0200

    Return results

--- api/neo4j.py
+++ api/neo4j.py
@@ -1,5 +1,8 @@
-from neo4j import GraphDatabase, Node
+from neo4j.v1 import GraphDatabase, Node
 from typing import List
+import logging
+
+logger = logging.getLogger('MetaExp.Neo4j')
 
 
 class Neo4j:
@@ -9,25 +12,31 @@ class Neo4j:
     def close(self):
         self._driver.close()
 
-    def start_precomputation(self, mode:str, length:int):
+    def start_precomputation(self, mode: str, length: int):
         # mode = {full, high-degree}
         with self._driver.session() as session:
             probably_json = session.run(
-                "Call computeAllMetaPaths($length);",
-                length = length)
-        
-    def get_metapaths(self, nodeset_A: List[Node], nodeset_B: List[Node], length:int):
+                "Call algo.computeAllMetaPaths($length);",
+                length=str(length))
+            return probably_json.records()
+
+    def get_metapaths(self, nodeset_A: List[int], nodeset_B: List[int], length: int):
+        nodeset_A = self._convert_node_set(nodeset_A)
+        nodeset_B = self._convert_node_set(nodeset_B)
+
         with self._driver.session() as session:
             probably_json = session.run(
-                "Call computeAllMetaPathsForInstances($startNodeIds, $endNodeIds, $length);",
-                startNodeIds = nodeset_A, endNodeIds = nodeset_B, length = length)
-        
+                "Call algo.computeAllMetaPathsForInstances($startNodeIds, $endNodeIds, $length);",
+                startNodeIds=nodeset_A, endNodeIds=nodeset_B, length=str(length))
+            return probably_json.records()
+
     def get_all_metapaths(self):
         with self._driver.session() as session:
             probably_json = session.run(
-                "Call readPrecomputedMetaPaths($filePath);",
-                filePath = "../../../precomputed/Precomputed_MetaPaths_BioL6.txt")
-        
+                "Call algo.readPrecomputedMetaPaths($filePath);",
+                filePath="../../../precomputed/Precomputed_MetaPaths_BioL6.txt")
+            return probably_json.single()
+
     # TODO: Implement different return types (node instances, node types)
     # TODO: What are the parameters of the neo4j procedure?
     def random_walk(self, maybe_start_id: int, maybe_number_of_random_walks: int, maybe_walk_length: int):
@@ -37,3 +46,13 @@ class Neo4j:
                 "CALL random_walk($maybe_start_id, $maybe_number_of_random_walks, $maybe_walk_length);",
                 maybe_start_id=maybe_start_id, maybe_number_of_random_walks=maybe_number_of_random_walks,
                 maybe_walk_length=maybe_walk_length)
+            return maybe_paths.records()
+
+    @staticmethod
+    def _convert_node_set(nodeset: List[int]) -> str:
+        str = "{"
+        for id in nodeset:
+            str += "{}, ".format(id)
+        str = str[:-2]
+        str += "}"
+        return str
