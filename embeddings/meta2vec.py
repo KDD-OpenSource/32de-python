b3207dc7408b99afff8d1d8a42a505c350cc79de
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2018-04-12 21:27:43 +0200

    Fix tensorflow code

--- embeddings/meta2vec.py
+++ embeddings/meta2vec.py
@@ -150,21 +150,21 @@ def model_word2vec(features, labels, mode, params):
     """
     input = tf.feature_column.input_layer(features, params['feature_columns'])
 
-    size_of_vocabulary = input.shape[1]
+    size_of_vocabulary = input.shape[1].value
 
     word_embeddings = tf.Variable(
         initial_value=tf.random_uniform(shape=[size_of_vocabulary, params['embedding_size']], minval=-1, maxval=1),
         name='word_embeddings')
 
     # Look up embedding for all words
-    embedded_words = tf.nn.embedding_lookup(word_embeddings, tf.argmax(input, axis=0))
+    embedded_words = tf.nn.embedding_lookup(word_embeddings, tf.argmax(input, axis=1))
 
     return _model_word2vec(mode, size_of_vocabulary, params['loss'], labels, embedded_words)
 
 
-def _model_word2vec(mode, size_of_vocabulary, loss, labels, embedded_words):
+def _model_word2vec(mode, size_of_vocabulary, loss: str, labels, embedded_words):
     # Concatenate vectors
-    concatenated_embeddings = embedded_words
+    concatenated_embeddings = tf.concat(tf.unstack(embedded_words, axis=0), axis=0)
 
     # Transform embeddings linearly
     hidden_layer = tf.layers.dense(inputs=concatenated_embeddings, units=size_of_vocabulary, activation=None,
@@ -174,6 +174,7 @@ def _model_word2vec(mode, size_of_vocabulary, loss, labels, embedded_words):
 
     # Apply softmax and calulate loss
     if loss == 'cross_entropy':
+        labels = tf.one_hot(indices=labels, depth=size_of_vocabulary)
         loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=hidden_layer, labels=labels),
                               name="cross_entropy_loss")
     else:
@@ -208,26 +209,32 @@ def model_paragraph_vectors_dbow():
     pass
 
 
-def create_estimator(model_dir, model_fn, input: Input):
+def create_estimator(model_dir, model_fn, input: Input, embedding_size: int, loss: str):
     features = tf.feature_column.categorical_column_with_identity('features',
-                                                                  num_buckets=input_fn.get_vocab_size())
-    run_config = tf.contrib.learn(gpu_memory_fraction=1,
-                                  tf_random_seed=42,
-                                  save_summary_steps=500,
-                                  save_checkpoints_steps=1000,
-                                  keep_checkpoint_max=5,
-                                  keep_checkpoint_every_n_hours=0.25,
-                                  log_step_count_steps=50)
+                                                                  num_buckets=input.get_vocab_size())
+    indicator_column = tf.feature_column.indicator_column(features)
+    run_config = tf.estimator.RunConfig(tf_random_seed=42,
+                                        save_summary_steps=500,
+                                        save_checkpoints_steps=1000,
+                                        keep_checkpoint_max=5,
+                                        keep_checkpoint_every_n_hours=0.25,
+                                        log_step_count_steps=50)
     classifier = tf.estimator.Estimator(
         model_fn=model_fn,
         model_dir=model_dir,
-        params={'feature_columns': [features]},
+        params={'feature_columns': [indicator_column],
+                'embedding_size': embedding_size,
+                'loss': loss},
         config=run_config)
     return classifier
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='')
+    parser.add_argument("--mode",
+                        choices=["train", "predict", "eval"],
+                        default="train",
+                        help="Specify what you want to do: 'train', 'predict' or 'eval'.")
     parser.add_argument('--input_type',
                         choices=["meta-paths", "nodes"],
                         help='Choose input type of data in json',
@@ -251,6 +258,15 @@ def parse_arguments():
                         help='Choose which model type should be used',
                         type=str,
                         required=True)
+    parser.add_argument('--embedding_size',
+                        help='Specify the size of the embedding',
+                        type=int,
+                        required=True)
+    parser.add_argument('--loss',
+                        choices=["cross_entropy"],
+                        help='Choose which loss should be used',
+                        type=str,
+                        required=True)
     return parser.parse_args()
 
 
@@ -260,14 +276,18 @@ def choose_function(model: str, model_type: str, input_type: str, json_path: str
     if input_type == "meta-paths":
         input = MetaPathsInput.from_json(json_data)
         if model_type == "bag-of-words":
+            input.bag_of_words_input()
             input_fn = input.bag_of_words_input
         elif model_type == "skip-gram":
+            input.skip_gram_input()
             input_fn = input.skip_gram_input
     elif input_type == "meta-nodes":
         input = NodeInput.from_json(json_data)
         if model_type == "bag-of-words":
+            input.bag_of_words_input()
             input_fn = input.bag_of_words_input
         elif model_type == "skip-gram":
+            input.skip_gram_input()
             input_fn = input.skip_gram_input
     if model == "word2vec":
         model_fn = model_word2vec
@@ -281,10 +301,12 @@ def choose_function(model: str, model_type: str, input_type: str, json_path: str
 
 if __name__ == "__main__":
     args = parse_arguments()
-    input, model_fn, input_fn = choose_function(model=args.model, model_type=args.model_type, input_type=args.input_type,
+    input, model_fn, input_fn = choose_function(model=args.model, model_type=args.model_type,
+                                                input_type=args.input_type,
                                                 json_path=args.json_path)
 
-    classifier = create_estimator(model_dir=args.model_dir, model_fn=model_fn, input=input)
+    classifier = create_estimator(model_dir=args.model_dir, model_fn=model_fn, input=input,
+                                  embedding_size=args.embedding_size, loss=args.loss)
 
     if args.mode == 'train':
         classifier.train(input_fn=input_fn)
