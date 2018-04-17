f70c3a7621a5a61bdc5cd76e661a2840735d2d24
Adrian Ziegler <adrian.ziegler@student.hpi.de>
2018-04-17 22:07:07 +0200

    hgetall has to be used because data type is HASH

--- api/redis.py
+++ api/redis.py
@@ -16,7 +16,7 @@ class Redis:
         return self._client.get("{}_{}:{}".format(data_set_name, start_type, end_type))
 
     def id_to_edge_type_map(self, data_set_name:str):
-        return self._client.get("{}_edge_type".format(data_set_name))
+        return self._client.hgetall("{}_edge_type".format(data_set_name))
 
     def id_to_node_type_map(self, data_set_name:str):
-        return self._client.get("{}_node_type".format(data_set_name))
+        return self._client.hgetall("{}_node_type".format(data_set_name))
