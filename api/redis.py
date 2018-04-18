a4d06a43334c3f89c2fddf778969a14df7d173d8
Adrian Ziegler <adrian.ziegler@student.hpi.de>
2018-04-18 14:02:53 +0200

    Change redis interface

--- api/redis.py
+++ api/redis.py
@@ -7,25 +7,26 @@ from util.config import REDIS_HOST, REDIS_PORT, REDIS_PASSWORD
 
 class Redis:
 
-    def __init__(self):
+    def __init__(self, data_set_name: str):
         self._client = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
+        self.data_set = data_set_name
         self.logger = logging.getLogger('MetaExp.{}'.format(__class__.__name__))
         self.logger.debug("current keys are: {}", self._client.keys())
 
-    def meta_paths(self, data_set_name: str, start_type: str, end_type: str) -> List:
+    def meta_paths(self, start_type: str, end_type: str) -> List:
         self.logger.debug("Retrieving meta paths...")
-        pickled_list = self._client.lrange("{}_{}_{}".format(data_set_name, start_type, end_type), 0, -1)
+        pickled_list = self._client.lrange("{}_{}_{}".format(self.data_set, start_type, end_type), 0, -1)
         self.logger.debug("Number of meta paths for {} and {} is {}".format(start_type, end_type, len(pickled_list)))
         return [pickle.loads(pickled_entry) for pickled_entry in pickled_list]
 
-    def id_to_edge_type_map(self, data_set_name: str):
-        return self._client.hgetall("{}_edge_type_mapping".format(data_set_name))
+    def id_to_edge_type_map(self):
+        return self._client.hgetall("{}_edge_type_mapping".format(self.data_set))
 
-    def id_to_node_type_map(self, data_set_name: str):
-        return self._client.hgetall("{}_node_type_mapping".format(data_set_name))
+    def id_to_node_type_map(self):
+        return self._client.hgetall("{}_node_type_mapping".format(self.data_set))
 
-    def node_type_to_id_map(self, data_set_name: str):
-        return self._client.hgetall("{}_node_type_mapping_reverse".format(data_set_name))
+    def node_type_to_id_map(self):
+        return self._client.hgetall("{}_node_type_mapping_reverse".format(self.data_set))
 
-    def edge_type_to_id_map(self, data_set_name: str):
-        return self._client.hgetall("{}_edge_type_mapping_reverse".format(data_set_name))
+    def edge_type_to_id_map(self):
+        return self._client.hgetall("{}_edge_type_mapping_reverse".format(self.data_set))
