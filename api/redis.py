c9a08ed88661cf563a1b5724b4edb8dc983be985
Adrian Ziegler <adrian.ziegler@student.hpi.de>
2018-04-18 11:15:23 +0200

    don't log pickled list but length of mp-list

--- api/redis.py
+++ api/redis.py
@@ -15,7 +15,7 @@ class Redis:
     def meta_paths(self, data_set_name: str, start_type: str, end_type: str) -> List:
         self.logger.debug("Retrieving meta paths...")
         pickled_list = self._client.lrange("{}_{}_{}".format(data_set_name, start_type, end_type), 0, -1)
-        self.logger.debug(pickled_list)
+        self.logger.debug("Number of meta paths for {} and {} is {}".format(start_type, end_type, len(pickled_list)))
         return [pickle.loads(pickled_entry) for pickled_entry in pickled_list]
 
     def id_to_edge_type_map(self, data_set_name: str):
