d89428659ca4e6614d2fb7f419e26711c3cf0188
Adrian Ziegler <adrian.ziegler@student.hpi.de>
2018-04-18 17:46:26 +0200

    Remove unused import

--- util/metapaths_database_importer.py
+++ util/metapaths_database_importer.py
@@ -1,12 +1,12 @@
-from tokenize import String, Number
 from util.datastructures import MetaPath
 from util.config import MAX_META_PATH_LENGTH, AVAILABLE_DATA_SETS
 from api.neo4j import Neo4j
 from api.redis import Redis
-from typing import Dict, List
+from typing import Dict, List, Tuple
 import logging
 import ast
 import pickle
+import embeddings
 
 class RedisImporter:
 
@@ -23,32 +23,36 @@ class RedisImporter:
         with Neo4j(bolt_url, username, password) as neo4j:
             for record in neo4j.get_meta_paths_schema(MAX_META_PATH_LENGTH):
                 meta_path_list = ast.literal_eval(record['metaPaths'])
+                self.logger.debug("Received list of strings {}".format(meta_path_list))
+                self.logger.debug("Number of meta paths is: {}".format(len(meta_path_list)))
+                meta_path_embedded_original = embeddings.meta2vec.calculate_embeddings(
+                    [mp.split("|") for mp in meta_path_list])
                 id_to_edge_type_dict = ast.literal_eval(record['edgesIDTypeDict'])
                 id_to_node_type_dict = ast.literal_eval(record['nodesIDTypeDict'])
                 self.logger.debug(type(meta_path_list))
                 self.logger.debug(type(id_to_edge_type_dict))
-                self.write_paths(meta_path_list)
+                self.write_paths(meta_path_embedded_original)
                 self.write_mappings(id_to_node_type_dict, id_to_edge_type_dict)
 
-    def write_paths(self, paths: List[str]):
-        self.logger.debug("Received list of strings {}".format(paths))
-        self.logger.debug("Number of meta paths is: {}".format(len(paths)))
-        for path in paths:
-            self.write_path(path)
+    def write_paths(self, paths_original_embedded: List[Tuple(List[str], List[float])]):
+        for path_original, path_embedded in paths_original_embedded:
+            self.write_path(path_original, path_embedded)
 
-    def write_path(self, path: str):
-        path_as_list = path.split("|")
-        start_node = path_as_list[0]
-        end_node = path_as_list[-1]
-        self.logger.debug("Adding metapath {} to record {}".format(path, "{}_{}_{}".format(self.redis.data_set, start_node, end_node)))
+    def write_path(self, path_original: List[str], path_embedded: List[float]):
+        start_node = path_original[0]
+        end_node = path_original[-1]
+        self.logger.debug("Adding metapath {} to record {}".format(path_original, "{}_{}_{}".format(self.redis.data_set,
+                                                                                                    start_node,
+                                                                                                    end_node)))
+        # Todo add embedding to MetaPath object.
         self.redis._client.lpush("{}_{}_{}".format(self.redis.data_set, start_node, end_node),
-                                  pickle.dumps(MetaPath(edge_node_list=path_as_list)))
+                                  pickle.dumps(MetaPath(edge_node_list=path_original)))
+        self.redis._client.lpush("{}_{}_{}_embedded".format(self.redis.data_set, start_node, end_node),
+                                  pickle.dumps(path_embedded))
 
     def write_mappings(self, node_type_mapping: Dict[int, str], edge_type_mapping: Dict[int, str]):
-            self.write_mapping("{}_node_type".format(self.redis.data_set), node_type_mapping)
-            self.write_mapping("{}_edge_type".format(self.redis.data_set), edge_type_mapping)
+        self.redis._client.hmset("{}_node_type".format(self.redis.data_set), node_type_mapping)
+        self.redis._client.hmset("{}_edge_type".format(self.redis.data_set), edge_type_mapping)
+        self.redis._client.hmset("{}_node_type_reverse".format(self.redis.data_set), {v: k for k,v in node_type_mapping.items()})
+        self.redis._client.hmset("{}_edge_type_reverse".format(self.redis.data_set), {v: k for k,v in edge_type_mapping.items()})
 
-    def write_mapping(self, key_name: str, mapping: Dict[int, str]):
-        key = "{}_mapping".format(key_name)
-        self.redis._client.hmset(key, mapping)
-        self.redis._client.hmset("{}_reverse".format(key), {v: k for k,v in mapping.items()})
