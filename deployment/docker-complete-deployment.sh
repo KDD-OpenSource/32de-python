3d9f9d2b2733732f49794becf3eb1cef43df9b89
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2018-06-26 11:40:32 +0200

    Add dev and production builds of ui

--- deployment/docker-complete-deployment.sh
+++ deployment/docker-complete-deployment.sh
@@ -27,8 +27,10 @@ cd ..
 git clone https://github.com/KDD-OpenSource/32de-UI.git
 cd 32de-UI/
 
-deployment/build-ui.sh . https://hpi.de/mueller/metaexp-demo-api/
-deployment/run-ui.sh
+export REACT_APP_API_HOST=${1:http://localhost:8000/}
+deployment/build-dev-ui.sh .
+deployment/build-production-ui.sh . ${1:http://localhost:8000/}
+deployment/run-production-ui.sh
 
 cd ..
 
